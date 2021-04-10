8cc0225ecb2a5ce415bbd15da5d9c09a9cbefdf6
Ilya <ilyamichlin1@gmail.com>
2021-04-10 18:32:12 +0300

    Update and Delete post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogPostView, BlogDetailedView, BlogCreateView
-
+from .views import BlogPostView, BlogDetailedView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailedView.as_view(), name='post_detail'),
     path('', BlogPostView.as_view(), name='home'),
